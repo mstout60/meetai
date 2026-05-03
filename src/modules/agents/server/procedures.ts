84280ccac2be86f853f6ed5f59cd2dcef0160a66
mstout60 <mstout@eprospector.com>
2026-05-02 22:35:22 -0500

    11: Agents form

--- src/modules/agents/server/procedures.ts
+++ src/modules/agents/server/procedures.ts
@@ -1,16 +1,37 @@
 import { db } from "@/db";
 import { agents } from "@/db/schema";
-import { createTRPCRouter, baseProcedure } from "@/trpc/init";
-import { TRPCError } from "@trpc/server";
+import { createTRPCRouter, protectedProcedure } from "@/trpc/init";
+import { agentInsertSchema } from "../schemas";
+import z from "zod";
+import { eq } from "drizzle-orm";
 
 export const agentsRouter = createTRPCRouter({
-  getMany: baseProcedure.query(async () => {
+  getOne: protectedProcedure
+    .input(z.object({ id: z.string() }))
+    .query(async ({ input }) => {
+      const [exisitingAgents] = await db
+        .select()
+        .from(agents)
+        .where(eq(agents, input.id));
+
+      return exisitingAgents;
+    }),
+  getMany: protectedProcedure.query(async () => {
     const data = await db.select().from(agents);
-
-    //await new Promise((resolve) => setTimeout(resolve, 5000));
-
-    //throw new TRPCError({ code: "BAD_REQUEST" });
 
     return data;
   }),
+  create: protectedProcedure
+    .input(agentInsertSchema)
+    .mutation(async ({ input, ctx }) => {
+      const [createdAgent] = await db
+        .insert(agents)
+        .values({
+          ...input,
+          userId: ctx.auth.user.id,
+        })
+        .returning();
+
+      return createdAgent;
+    }),
 });
