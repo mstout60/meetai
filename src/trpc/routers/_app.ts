6a2c90d1f6518bf383db537798a90c75a5a705c8
mstout60 <mstout@eprospector.com>
2026-05-11 16:42:30 -0500

    16: Meetings setup

--- src/trpc/routers/_app.ts
+++ src/trpc/routers/_app.ts
@@ -1,8 +1,10 @@
 import { agentsRouter } from "@/modules/agents/server/procedures";
 
 import { createTRPCRouter } from "../init";
+import { meetinsRouter } from "@/modules/meetings/server/proedures";
 export const appRouter = createTRPCRouter({
   agents: agentsRouter,
+  meetings: meetinsRouter,
 });
 // export type definition of API
 export type AppRouter = typeof appRouter;
